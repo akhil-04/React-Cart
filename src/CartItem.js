37a4dd4e1f32c1d064f782d6682a21ca2d882f5b
akhil-04 <svam1008@gmail.com>
2021-01-11 12:25:33 +0530

    we made state and used that in our JSX

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,19 +2,33 @@ import React from 'react';
 
 
 class CartItem extends React.Component{
+    //state
+    constructor(){
+        super();
+        this.state= {
+            price:999,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+    }
     render(){
+        const {price, qty, title} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs 999</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs {price}</div>
+                    <div style={{color:'#777'}}>Qty: {qty}</div>
 
                     <div className="cart-item-actions">
                         {/* Buttons */ }
+                        <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg" />
+                        <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg" />
+                        <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/3096/3096673.svg" />
                     </div>
                 </div>
             </div>
